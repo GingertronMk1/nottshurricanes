9b24519134ba49daa8ab6258a39e3cdd56bc2681
Jack Ellis <jackellis1504@gmail.com>
2026-09-14 19:58:29 +0100

    pictures
     M resources/js/pages/Welcome.vue
    ?? resources/images/
    ?? resources/js/components/LeftAndRight.vue

---
+++ resources/js/components/LeftAndRight.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+</script>
+<template>
+    <div class="flex flex-col *:flex *:flex-row nth-2:flex-row-reverse [&>*>*]:max-w-1/2 [&>*>*]:flex-1">
+        <slot />
+    </div>
+</template>
--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -1,12 +1,35 @@
 <script setup lang="ts">
 import { Head } from '@inertiajs/vue3';
 import AppLayout from "@/layouts/AppLayout.vue";
+import LeftAndRight from '@/components/LeftAndRight.vue';
 </script>
 
 <template>
     <Head title="Welcome" />
     <AppLayout>
-        The Nottinghamshire Hurricanes RFC are Nottingham's first and only LGBTQ+ inclusive rugby club, established in 2019.
-        Open to anyone aged 18 and over, the team welcomes players of all abilities, genders, and sexualities for both touch and contact rugby.
+        <LeftAndRight>
+            <div>
+                <div class="flex flex-col">
+                    <h1 class="text-3xl font-hurricanes-header">Who are we?</h1>
+                    <p>
+                        We are the Nottinghamshire Hurricanes, Nottingham's first and only LGBTQ+ inclusive rugby club.
+                        We compete in the IGR North league in touch rugby, as well as training for rugby union.
+                    </p>
+                    <p>
+                        We welcome players of all abilities, genders, and sexualities for both touch and contact rugby.
+                        So long as you're over 18, the risk assessments are a bugger otherwise.
+                    </p>
+                </div>
+                <img src="../../images/ro_in_manchester.jpg" alt="Ro in Manchester" />
+            </div>
+            <div>
+                <img src="../../images/bas_looking_stern.jpg" alt="Bas looking stern" />
+                <div class="flex-col">
+                    <h1 class="text-3xl font-hurricanes-header">
+                        We serve face
+                    </h1>
+                </div>
+            </div>
+        </LeftAndRight>
     </AppLayout>
 </template>
