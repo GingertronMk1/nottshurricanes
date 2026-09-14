53b6c1355ad2b3158f2942e4875d8a17765cab30
Jack Ellis <jackellis1504@gmail.com>
2026-09-14 18:29:45 +0100

    update fonts
     M resources/css/fonts.css
     M resources/js/components/AppHeader.vue

--- resources/js/components/AppHeader.vue
+++ resources/js/components/AppHeader.vue
@@ -9,11 +9,11 @@ import {aboutUs, home } from "@/routes";
 <template>
     <header class="flex flex-row justify-center py-2 font-hurricanes-header">
         <div class="container px-2 flex flex-row justify-between items-stretch ">
-            <Link :href="home()" class="text-2xl">Nottinghamshire Hurricanes</Link>
-            <div class="flex flex-row items-center gap-x-2 *:p-2 *:rounded-sm">
-                <Link class="hover:bg-gray-100" :href="touchTraining()">Touch Rugby</Link>
-                <Link class="hover:bg-gray-100" :href="unionTraining()">Rugby Union</Link>
-                <Link class="hover:bg-gray-100" :href="aboutUs()">About Us</Link>
+            <Link :href="home()" class="text-4xl py-2 hover:text-hurricanes-purple">Nottinghamshire Hurricanes</Link>
+            <div class="flex flex-row items-center gap-x-2 *:p-2 *:rounded-sm *:hover:bg-gray-100 *:hover:text-hurricanes-purple">
+                <Link :href="touchTraining()">Touch Rugby</Link>
+                <Link :href="unionTraining()">Rugby Union</Link>
+                <Link :href="aboutUs()">About Us</Link>
             </div>
         </div>
     </header>
