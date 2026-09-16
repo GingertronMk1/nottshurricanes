242182b1f072508fa64260b483f18f1a818718f5
Jack Ellis <jackellis1504@gmail.com>
2026-09-16 17:58:13 +0100

    Stylistic update

--- resources/js/pages/ExternalLinks.vue
+++ resources/js/pages/ExternalLinks.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import AppLayout from '@/layouts/AppLayout.vue';
+import MarkdownRenderer from "@/components/MarkdownRenderer.vue";
 
 defineProps<{
     links: {
@@ -11,16 +12,19 @@ defineProps<{
 
 <template>
     <AppLayout>
-        <h2 class="font-hurricanes-header text-3xl">External Links</h2>
-        <div class="mt-2 flex flex-col gap-2 pl-2">
-            <a
-                v-for="link in links"
-                :key="link.href"
-                class="hover:text-hurricanes-purple"
-                :href="link.href"
-                target="_blank"
-                v-text="link.name"
-            />
+        <div class="flex flex-col gap-2">
+            <h2 class="font-hurricanes-header text-3xl">External Links</h2>
+            <MarkdownRenderer/>
+            <div class="mt-2 flex flex-col gap-2 pl-2">
+                <a
+                    v-for="link in links"
+                    :key="link.href"
+                    class="hover:text-hurricanes-purple"
+                    :href="link.href"
+                    target="_blank"
+                    v-text="link.name"
+                />
+            </div>
         </div>
     </AppLayout>
 </template>
