31d047ac87fdae5f43b8c0729f48e274cb117fff
Jack Ellis <jackellis1504@gmail.com>
2026-09-16 15:35:39 +0100

    lil bitta column work and we're done for committee
     M app/Filament/Resources/CommitteeMembers/Schemas/CommitteeMemberForm.php

--- app/Filament/Resources/CommitteeMembers/Schemas/CommitteeMemberForm.php
+++ app/Filament/Resources/CommitteeMembers/Schemas/CommitteeMemberForm.php
@@ -2,7 +2,9 @@
 
 namespace App\Filament\Resources\CommitteeMembers\Schemas;
 
+use App\Models\User;
 use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
@@ -14,21 +16,28 @@ class CommitteeMemberForm
     public static function configure(Schema $schema): Schema
     {
         return $schema
+            ->columns(4)
             ->components([
                 TextInput::make('role')
+                    ->columnSpan(2)
                     ->required(),
                 TextInput::make('name')
+                    ->columnSpan(2)
                     ->required(),
                 Textarea::make('description')
                     ->columnSpanFull(),
                 Toggle::make('is_active')
-                    ->required(),
+                    ->columnSpan(1),
                 TextInput::make('sort_order')
+                    ->columnSpan(1)
                     ->numeric(),
-                TextInput::make('user_id')
-                    ->numeric(),
+                Select::make('user_id')
+                    ->options(User::query()->pluck('name', 'id'))
+                    ->columnSpan(1)
+                    ->label('User (optional)'),
                 Group::make()
                     ->relationship('profilePicture')
+                    ->columnSpan(2)
                     ->schema([
                         FileUpload::make('filename'),
                     ]),
