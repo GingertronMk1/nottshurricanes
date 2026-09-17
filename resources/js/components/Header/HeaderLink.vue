5c0cb0af3c72bcfbbbeae1f5de36bba6982d7a95
Jack Ellis <jackellis1504@gmail.com>
2026-09-17 17:25:30 +0100

    Feature/better header (#5)

---
+++ resources/js/components/Header/HeaderLink.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { Link } from '@inertiajs/vue3';
+import type { HeaderItem, HeaderItemLink } from '@/types';
+import { RouteDefinition } from '@/wayfinder';
+
+defineProps<{
+    link: HeaderItemLink;
+}>();
+</script>
+
+<template>
+    <Link
+        v-if="(link.path as RouteDefinition<'get'>).method !== undefined"
+        class="hover:text-hurricanes-purple hover:bg-gray-100"
+        :href="link.path"
+    >
+        {{ link.title }}
+    </Link>
+    <a
+        v-else
+        class="hover:text-hurricanes-purple hover:bg-gray-100"
+        :href="link.path as string"
+        target="_blank"
+        v-text="link.title"
+    />
+</template>
