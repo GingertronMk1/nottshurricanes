bb5524e7b948fb4f061042ebd885a3c6445e3e7a
Jack Ellis <jackellis1504@gmail.com>
2026-09-17 19:55:34 +0100

    add filament fixture
    ?? app/Filament/Resources/Fixtures/

---
+++ app/Filament/Resources/Fixtures/Schemas/FixtureForm.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Filament\Resources\Fixtures\Schemas;
+
+use App\RugbyType;
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+
+class FixtureForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                Select::make('type')
+                    ->options(RugbyType::class)
+                    ->required(),
+                TextInput::make('opposition')
+                    ->required(),
+                TextInput::make('location')
+                    ->required(),
+                DateTimePicker::make('start')
+                    ->required(),
+            ]);
+    }
+}
